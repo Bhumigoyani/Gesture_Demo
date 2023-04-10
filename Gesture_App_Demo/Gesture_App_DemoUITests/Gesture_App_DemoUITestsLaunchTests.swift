3e6d048fa142e947305411245483ce379b1eb642
Bhumigoyani <128502399+Bhumigoyani@users.noreply.github.com>
2023-04-10 11:36:53 -0400

    ViewController with Class name

---
+++ Gesture_App_Demo/Gesture_App_DemoUITests/Gesture_App_DemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Gesture_App_DemoUITestsLaunchTests.swift
+//  Gesture_App_DemoUITests
+//
+//  Created by Goyani Bhumi Nitesh on 2023-04-10.
+//
+
+import XCTest
+
+final class Gesture_App_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
